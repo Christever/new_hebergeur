cb3cb5573f05a0ef415aa70fe6bb8729ba8f2e7e
Christever <christian@steverlynck.fr>
2026-03-30 13:17:50 +0200

    maj

--- public/index.php
+++ public/index.php
@@ -1,13 +1,40 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="css/style.css" rel="stylesheet"/>
+    <link href="css/style.css" rel="stylesheet" />
+    <link href="css/all.min.css" rel="stylesheet" />
+    <link rel="icon" type="image/png" href="images/favicon.png">
+    <!-- <link href="css/font_awasome_all.min.css" rel="stylesheet"/> -->
 </head>
+
 <body>
-    <h1 class="text-3xl text-center">Hello</h1>
-    <button class="btn btn-secondary">Ok</button>
+    <header>
+        <a href="../">
+            <span>ShareFILES</span>
+        </a>
+    </header>
+    <section>
+        <h1>
+            <i class="fas fa-paper-plane"></i>
+        </h1>
+
+        <form method="post" action="" enctype="multipart/form-data">
+            <p>
+                <label class="text-lg text-center mb-2" for="image">Sélectionnez votre fichier</label>
+                <input type="file" name="image" id="image">
+            </p>
+            <p id="send">
+                <button type="submit">
+                    Envoyer
+                    <i class="fas fa-long-arrow-alt-right"></i>
+                </button>
+            </p>
+        </form>
+    </section>
 </body>
+
 </html>
